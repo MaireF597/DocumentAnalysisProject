2a1684a8296157f4e846bb3b4fcd04773f70f1be
Marie <maireadfinnerty@gmail.com>
2024-04-11 10:51:56 -0500

    num_of_paragraph started and num_of_words complete

--- Untitled-1.py
+++ Untitled-1.py
@@ -25,5 +25,31 @@ def num_of_characters(file_name):
                 count+=1
     return count
 
+def num_of_words(file_name):
+    count = 0
+    var = open(file_name, "r")
+    for line in var:
+        for letter in line:
+            if letter==" ":
+                count+=1
+    if (count>1):
+        count+=1
+    return count
+
+def num_of_paragraphs(file_name):
+    count = 0
+    var = open(file_name, "r")
+    for line in var:
+        for letter in line:
+            if letter=="\t":
+                count+=1
+    return count
+
+
+def sentences_per_paragraph():
+    pass
+
 print(num_of_sentences("testFile.txt"))
 print(num_of_characters("testFile.txt"))
+print(num_of_words("testFile.txt"))
+print(num_of_paragraphs("testFile.txt"))
