eb7fcaefc6466e1f43d8d0a76fe55788440e715a
Marie <maireadfinnerty@gmail.com>
2024-04-14 21:37:13 -0500

    Updates to methods

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,4 @@
 
-
-
-def essay_reader(file_name):
-    number = 0
-    num_of_sentences(file_name)
-
-
-
 def num_of_sentences(file_name):
     count = 0
     var = open(file_name, "r")
@@ -26,18 +18,20 @@ def num_of_characters(file_name):
     return count
 
 def num_of_words(file_name):
+    last_letter=""
     count = 0
     var = open(file_name, "r")
     for line in var:
         for letter in line:
-            if letter==" ":
-                count+=1
+                if letter==" " and last_letter!=(".") and last_letter!=("?") and last_letter!=("!"):
+                    count+=1
+                last_letter=letter
     if (count>1):
         count+=1
     return count
 
 def num_of_paragraphs(file_name):
-    count = 0
+    count = 1
     var = open(file_name, "r")
     for line in var:
         for letter in line:
@@ -46,7 +40,21 @@ def num_of_paragraphs(file_name):
     return count
 
 
-
+def sentences_per_paragraph(file_name):
+    array = []
+    count=0
+    var = open(file_name, "r")
+    for line in var:
+        for letter in line:
+            if (letter=="\n"):
+                array.append(count)
+            elif (letter==(".") or letter==("?") or letter==("!")):
+                count+=1
+    array.append(count)
+    sum = 0
+    for val in array:
+        sum=sum+val
+    return (sum/(len(array)))
 
 def words_per_sentence(file_name):
     array = []
@@ -55,16 +63,16 @@ def words_per_sentence(file_name):
     for line in var:
         for letter in line:
             if (letter=="\n"):
-                count=-1
-            if (letter==(".") or letter==("?") or letter==("!")):
-                array.append(count+1)
                 count=0
+            if (letter==(".") or letter==("?") or letter==("!")):
+                array.append(count)
+                count=-1
             elif letter==" ":
                 count+=1
     sum = 0
     for val in array:
         sum=sum+val
-    return (sum/(len(array)))
+    return (round(sum/(len(array)),1))
 
 
 
@@ -74,30 +82,82 @@ def characters_per_word(file_name):
     var = open(file_name, "r")
     for line in var:
         for letter in line:
-            if (letter==(" ")):
-                array.append(count+1)
+            if (letter=="\n"):
+                count=0
+            if (letter==(".") or letter==("?") or letter==("!")):
+                array.append(count)
+                count=-1
+            if (letter==(" ") and count!=0):
+                array.append(count)
                 count=0
             else:
-                count+=1
+                if (letter!=","):
+                    count+=1
     sum = 0
     for val in array:
         sum=sum+val
-    return (sum/(len(array)))
+    return (round(sum/(len(array)),1))
 
-def flesch_reading_ease():
+
+def syllable_counter(file_name):
     pass
 
-def fleschkincaid_grade_level():
-    pass
 
-def sentences_per_paragraph():
+def flesch_reading_ease(file_name):
     pass
+    # 206.835 - (1.015 * ASL) - (84.6 * ASW)
+    # ASL - average sentence length (num of words/num of sentences)
+    # ASW - average num of syllables per word (num of syllables / num of words)
+    syll_sum = 0.0
+    word = ""
+    var = open(file_name, "r")
+    for line in var:
+        for letter in line:
+            if (letter==" " or letter=="\n" or letter==(".") or letter==("?") or letter==("!")):
+                var = (syllable_counter(word))
+                
+                syll_sum +=(var)
+                word=""
+            else:
+                word+=letter
+
+    ASL = num_of_words(file_name) / num_of_sentences(file_name)
+    ASW = syll_sum / num_of_words(file_name)
+    return (206.835 - (1.015 * ASL) - (84.6 * ASW))
+
+def fleschkincaid_grade_level(file_name):
+    pass
+    # 20(.39*ASL)+(11.8*ASW) - 15.59
+    # ASL - average sentence length (num of words/num of sentences)
+    # ASW - average num of syllables per word (num of syllables / num of words)
+    syll_sum = 0
+    word = ""
+    var = open(file_name, "r")
+    for line in var:
+        for letter in line:
+            if (letter==" " or letter=="\n" or letter==(".") or letter==("?") or letter==("!")):
+                syll_sum = syll_sum + syllable_counter(word)
+                word=""
+            else:
+                word+letter
+
+    ASL = num_of_words(file_name) / num_of_sentences(file_name)
+    ASW = syll_sum / num_of_words(file_name)
+    return (.39*ASL)+(11.8*ASW) - 15.59
 
 def passive_sentences():
     pass
 
-#print(num_of_sentences("testFile.txt"))
-#print(num_of_characters("testFile.txt"))
-#print(num_of_words("testFile.txt"))
-#print(num_of_paragraphs("testFile.txt"))
-print(words_per_sentence("testFile.txt"))
+
+def essay_reader(file_name):
+    print("Words: " ,num_of_words(file_name))
+    print("Characters: ", num_of_characters(file_name))
+    print("paragraphs: ",num_of_paragraphs(file_name))
+    print("sentences: ",num_of_sentences(file_name))
+    print("sentences per paragraph: ",sentences_per_paragraph(file_name))
+    print("words per sentence: ",words_per_sentence(file_name))
+    print("character per word: ",characters_per_word(file_name))
+    #print("flesch_reading_ease: ",flesch_reading_ease(file_name))
+    #print("fleschkincaid_grade_level: ",fleschkincaid_grade_level(file_name))
+
+essay_reader("testFile.txt")
